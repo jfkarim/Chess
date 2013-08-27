d334925e27c1031a3d0f502572e8263503b36685
App Academy Student <student@appacademy.io>
2013-08-27 10:51:09 -0400

    Created Knight class and created Bishop class.

---
+++ knight.rb
@@ -0,0 +1,11 @@
+require_relative 'piece'
+
+class Knight < Piece
+
+  MOVE_INCREMENT =  [ [-2, -1], [-2,  1],[-1, -2],[-1,  2],[ 1, -2], [ 1,  2],                      [ 2, -1], [ 2,  1] ]
+
+  def initialize (start_pos, color=nil)
+    super("N", start_pos, MOVE_INCREMENT, color=nil)
+  end
+
+end
