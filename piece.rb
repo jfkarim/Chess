e67ba04290292d84068ad01305d4442418d827c6
App Academy Student <student@appacademy.io>
2013-08-27 10:18:34 -0400

    Created Piece class. Created valid_moves class.

--- piece.rb
+++ piece.rb
@@ -1,11 +1,27 @@
 class Piece
 
+  attr_accessor :type, :start_pos, :move_possibilities, :color
 
-
-  def initialize(type, start_pos, move_possibility, color=nil)
+  def initialize(type, start_pos, move_possibilities, color=nil)
     self.type = type #Symbol used to refer to piece on the board
     self.start_pos = start_pos
-    self.move_possibility = move_possibility
+    self.move_increments = move_increments #array of possible moves, i.e.,
+  end
+
+  def raw_possible_moves(start_pos)
+    raw_possible_moves = []
+
+    current_row, current_col = start_pos
+
+    move_increments.each do |(d_row, d_col)|
+
+      possible_move = [current_row + d_row, current_col + d_col]
+
+      raw_possible_moves << new_pos
+      #need to check if pieces in way, pos move on board, and if king check
+    end
+
+    valid_moves
   end
 
 
